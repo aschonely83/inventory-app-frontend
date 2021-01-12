babcb8f70f60e2eaea6c34092508f8bf148d780d
aschonely83 <aschonely@gmail.com>
2021-01-12 18:23:41 -0500

    created GET request for retailers

--- js/index.js
+++ js/index.js
@@ -3,5 +3,31 @@ class InventoryApp {
     this.name = name
   }
 
-   
+  static container() {
+    return this.c ||= document.querySelector('#lists')
+  }
+
+  static all() {
+    return fetch("http://localhost:3000/retailers")
+      .then(res => res.json())
+      .then(retailerList => {
+        this.collection = retailerList.map(rAttributes => new RetailerList(rAttributes))
+        let listItems = this.collection.map(list => list.render())
+        this.container().append(...listItems)
+        return this.collection
+      })
+  }
+
+  render() {
+    this.element ||= document.createElement('li');
+
+    this.element.classList.add('nameList');
+    this.name ||= document.createElement('p');
+    this.name.classList.add('name');
+    this.name.dataset.retailerId = this.retailerId;
+    this.name.textContent = this.name;
+
+    this.element.append(this.name);
+    return this.element;
+  } 
 }
