e7f0ce9843445a0257961b69921d9136a73ff446
aschonely83 <aschonely@gmail.com>
2021-01-12 19:24:16 -0500

    set up DOM content loader

--- js/index.js
+++ js/index.js
@@ -1,4 +1,4 @@
-class InventoryApp {
+class RetailerList {
   constructor(name){
     this.name = name
   }
--- js/listeners.js
+++ js/listeners.js
@@ -1,3 +1,3 @@
 document.addEventListener('DOMContentLoaded', function(e) {
-    TodoList.all()
+    RetailerList.all()
   })
