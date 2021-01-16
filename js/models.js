9d4025b92a996551b126d57d950d85964b367f95
aschonely83 <aschonely@gmail.com>
2021-01-16 12:10:54 -0500

    set up retailer class

--- js/models.js
+++ js/models.js
@@ -1,50 +1,19 @@
-const BASE_URL = "http://localhost:3000"
-const RETAILERS_URL = `${BASE_URL}/retailers`
-const PALLETS_URL = `${BASE_URL}/pallets`
-
-document.addEventListener("DOMContentLoaded", () => {
- getRetailer();
- getPallet();  
-});
-
-function getRetailer() {
-  fetch(RETAILERS_URL)
-  .then(resp => resp.json())
-  .then(retailers => { 
-    retailers.data.forEach(retailer => {
-      makeRetailerList(retailer)
-    })
-  })
-}
-
-function makeRetailerList(retailer) {
-  const retailerList = document.querySelector('#lists')
-  const retailerElement = document.createElement("h4")
+class Retailer {
   
-  retailerElement.id =`retailer-name-${retailer.id}`
-  retailerElement.innerText = retailer.attributes.name
-  
-  retailerList.appendChild(retailerElement)
-}
+  constructor(attributes) {
+    let whiteList = ["id", "name"]
+    whiteList.forEach(attr => this[attr] = attributes[attr])
+  }
 
-function getPallet() {
-  fetch(PALLETS_URL)
-  .then(resp => resp.json())
-  .then(pallets => { 
-    pallets.data.forEach(pallet => {
-      makePalletList(pallet)
-    })
-  })
-}
+  static container() {
+    return this.c ||= document.querySelector("#retailersContainer")
+  }
 
-function makePalletList(pallet) {
-  const retailerList = document.querySelector('#lists')
-  const palletElement = document.createElement("p")
-  
-  retailerList.append(palletElement)
+  static list() {
+    return this.l ||= document.querySelector("#retailers")
+  }
 
-  palletElement.id =`pallet-boxes-${pallet.id}`
-  palletElement.innerText = pallet.attributes.boxes
-  
-  
+  static all() {
+    fetch("http://localhost:3000/retailers")
+  }
 }
