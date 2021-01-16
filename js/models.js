6b4a540021dacdeb6b7a57fae17681bd7104484e
aschonely83 <aschonely@gmail.com>
2021-01-16 12:17:25 -0500

    create render method

--- js/models.js
+++ js/models.js
@@ -16,4 +16,16 @@ class Retailer {
   static all() {
     fetch("http://localhost:3000/retailers")
   }
+
+  render() {
+    this.element ||= document.createElement('li');
+
+    this.element.classList.add(..."my-2 px-4 bg-blue-200 grid grid-cols-12 sm:grid-cols-6".split(" "));
+    this.retailerName ||= document.createElement('p');
+    this.retailerName.classList.add(..."py-4 col-span-10 sm:col-span-4".split(" "));
+    this.retailerName.textContent = this.name;
+
+    this.element.append(this.retailerName);
+    return this.element;
+  }
 }
